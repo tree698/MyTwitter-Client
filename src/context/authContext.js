587281d1364a3cfc327cc6fe52cf66d7a80114f0
Chanwoo <tree698@gmail.com>
2022-08-02 17:23:31 +0900

    add context & service

---
+++ src/context/authContext.js
@@ -0,0 +1,87 @@
+import {
+  createContext,
+  createRef,
+  useCallback,
+  useContext,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useState,
+} from 'react';
+import Header from '../components/header';
+import Login from '../pages/login';
+
+const AuthContext = createContext();
+
+const contextRef = createRef();
+
+export function AuthProvider({ authService, authErrorEventBus, children }) {
+  const [user, setUser] = useState(undefined);
+
+  useImperativeHandle(contextRef, () => (user ? user.token : undefined));
+
+  useEffect(() => {
+    authErrorEventBus.listen((err) => {
+      setUser(undefined);
+    });
+  }, [authErrorEventBus]);
+
+  useEffect(() => {
+    authService.me().then(setUser).catch(console.error);
+  }, [authService]);
+
+  const signup = useCallback(
+    async (username, password, name, email, url) =>
+      authService
+        .signup(username, password, name, email, url)
+        .then((user) => setUser(user)),
+    [authService]
+  );
+
+  const login = useCallback(
+    async (username, password) =>
+      authService.login(username, password).then((user) => setUser(user)),
+    [authService]
+  );
+
+  const logout = useCallback(
+    async () => authService.logout().then(() => setUser(undefined)),
+    [authService]
+  );
+
+  const context = useMemo(
+    () => ({
+      user,
+      login,
+      logout,
+      signup,
+    }),
+    [user, login, logout, signup]
+  );
+
+  return (
+    <AuthContext.Provider value={context}>
+      {user ? (
+        children
+      ) : (
+        <div className="app">
+          <Header />
+          <Login onSignup={signup} onLogin={login} />
+        </div>
+      )}
+    </AuthContext.Provider>
+  );
+}
+
+export class AuthErrorEventBus {
+  listen(callback) {
+    this.callback = callback;
+  }
+  notify(error) {
+    this.callback(error);
+  }
+}
+
+export default AuthContext;
+export const fetchToken = () => contextRef.current;
+export const useAuth = () => useContext(AuthContext);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AuthService from './service/auth';
+import TweetService from './service/tweet';
+import { AuthErrorEventBus, AuthProvider } from './context/authContext';
+
+const baseURL = process.env.REACT_APP_BASE_URL;
+const authErrorEventBus = new AuthErrorEventBus();
+const authService = new AuthService();
+const tweetService = new TweetService(baseURL);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider
+      authService={authService}
+      authErrorEventBus={authErrorEventBus}
+    >
+      <App tweetService={tweetService} />
+    </AuthProvider>
   </React.StrictMode>
 );
