3d26fb3f506557d43e8380ec9adaaabcdcc0f1cf
Chanwoo <tree698@gmail.com>
2022-08-18 23:08:58 +0900

    modify service/auth.js

--- src/network/http.js
+++ src/network/http.js
@@ -1,6 +1,7 @@
 export default class HttpClient {
-  constructor(baseURL) {
+  constructor(baseURL, authErrorEvent) {
     this.baseURL = baseURL;
+    this.authErrorEvent = authErrorEvent;
   }
 
   async fetch(url, options) {
@@ -19,7 +20,12 @@ export default class HttpClient {
     if (response.status > 299 || response.status < 200) {
       const message =
         data && data.message ? data.message : 'Something went wrong';
-      throw new Error(message);
+      const error = new Error(message);
+      if (response.status === 401) {
+        this.authErrorEvent.notify(error);
+        return;
+      }
+      throw error;
     }
     return data;
   }
--- src/service/tweet.js
+++ src/service/tweet.js
@@ -1,16 +1,21 @@
 export default class TweetService {
-  constructor(http) {
+  constructor(http, tokenStorage) {
     this.http = http;
+    this.tokenStorage = tokenStorage;
   }
 
   async getTweets(username) {
     let query = username ? `?username=${username}` : '';
-    return this.http.fetch(`/tweets${query}`, { method: 'GET' });
+    return this.http.fetch(`/tweets${query}`, {
+      method: 'GET',
+      headers: this.getHeaders(),
+    });
   }
 
   async postTweet(text) {
     return this.http.fetch('/tweets', {
       method: 'POST',
+      headers: this.getHeaders(),
       body: JSON.stringify({
         text,
         username: 'ellie',
@@ -20,13 +25,22 @@ export default class TweetService {
   }
 
   async deleteTweet(tweetId) {
-    return this.http.fetch(`/tweets/${tweetId}`, { method: 'DELETE' });
+    return this.http.fetch(`/tweets/${tweetId}`, {
+      method: 'DELETE',
+      headers: this.getHeaders(),
+    });
   }
 
   async updateTweet(tweetId, text) {
     return this.http.fetch(`/tweets/${tweetId}`, {
       method: 'PUT',
+      headers: this.getHeaders(),
       body: JSON.stringify({ text }),
     });
+  }
+
+  getHeaders() {
+    const token = this.tokenStorage.getToken();
+    return { Authorization: `Bearer ${token}` };
   }
 }
